53f04ba7a134b186f8d2d4160d9d9c8b0eacf955
MugiSus <mmgiss256@gmail.com>
2024-05-28 06:58:28 +0900

    address server errors

--- components/geoDateDisplay.tsx
+++ components/geoDateDisplay.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-import useGeolocationEvents from "beautiful-react-hooks/useGeolocationEvents";
 import { useEffect, useState } from "react";
 import DateDisplay from "./dateDisplay";
 import fetchNTP from "@/lib/fetchNtp";
 
 export default function GeoDateDisplay({ initDate }: { initDate: number }) {
   const [longitude, setLongitude] = useState(0);
-  const { isSupported, onChange, onError } = useGeolocationEvents({
-    enableHighAccuracy: true,
-    timeout: 60000,
-    maximumAge: 0,
-  });
-
-  onChange((geo) => isSupported && setLongitude(geo.coords.longitude));
 
   const [ntpOffset, setNtpOffset] = useState(0);
 
   useEffect(() => {
     fetchNTP().then((offset) => setNtpOffset(offset));
+
+    const isGeolocationSupported =
+      typeof window !== "undefined" && "geolocation" in window.navigator;
+
+    if (!isGeolocationSupported) return;
+
+    navigator.geolocation.watchPosition(
+      (position) => {
+        setLongitude(position.coords.longitude);
+      },
+      (error) => {
+        console.error(error);
+      },
+      {
+        maximumAge: 0,
+        timeout: 20000,
+        enableHighAccuracy: true,
+      }
+    );
   }, []);
 
   const timeDifference = (longitude / 15) * 60 * 60 * 1000 + ntpOffset;
