e7abcb4208e272d4800c8fd1b1a3b7b31d8c60a8
MugiSus <mmgiss256@gmail.com>
2024-05-28 18:14:10 +0900

    edit description

--- app/page.tsx
+++ app/page.tsx
@@ -16,23 +16,25 @@ export default function Home() {
       <div className="flex min-h-lvh items-center justify-center snap-center flex-col gap-16 font-light text-center px-8 sm:px-16 py-16">
         <h1 className="font-semibold text-3xl">The Most Accurate Clock</h1>
         <div className="flex flex-col gap-4 items-center leading-relaxed">
-          <p className="text-cyan-600 font-bold">
+          <p className="text-cyan-600 font-semibold">
             The most accurate clock provides you with the most accurate time for
             your location.
           </p>
           <p>
             Since{" "}
-            <span className="text-pink-600 font-bold">The left red clock</span>{" "}
-            shows your timezone&apos;s NTP server time, which is, sure,
-            accurate,{" "}
-            <span className="text-cyan-600 font-bold">the blue one</span> is the
-            most accurate, with the power of your GPS location information.
+            <span className="text-pink-600 font-semibold">
+              The left red clock
+            </span>{" "}
+            shows NTP server time, which is, sure, accurate,{" "}
+            <span className="text-cyan-600 font-semibold">the blue one</span>{" "}
+            provides the most accurate, with the power of your GPS location
+            information.
           </p>
           <p>
             This calculates an actual time difference, for example if you are at
             5° latitude, it advances{" "}
-            <span className="text-cyan-600 font-bold">the blue clock</span> by
-            20 minutes.
+            <span className="text-cyan-600 font-semibold">the blue clock</span>{" "}
+            by 20 minutes.
           </p>
           <p>Kyoto and Tokyo have 15 minutes of a time difference of approx.</p>
           <Link
