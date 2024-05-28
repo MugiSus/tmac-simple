c38223822f8e3177c7acc6d295659532bc7770d2
MugiSus <mmgiss256@gmail.com>
2024-05-28 18:29:32 +0900

    change clock height

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col bg-slate-950">
-      <div className="flex min-h-lvh items-center justify-center flex-wrap py-16 snap-center">
+      <div className="flex min-h-svh items-center justify-center flex-wrap py-16 snap-center">
         <div className="text-pink-600">
           <NTPDateDisplay initDate={Date.now()} />
         </div>
